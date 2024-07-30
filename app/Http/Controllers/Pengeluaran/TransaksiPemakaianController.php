6bd98696f7c45dd60978e5f582f70f4929762d7c
alpiansu <alpiansusanto.idm@gmail.com>
2024-07-30 14:48:14 +0700

    init controller untuk page pengeluaran

---
+++ app/Http/Controllers/Pengeluaran/TransaksiPemakaianController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Pengeluaran;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TransaksiPemakaianController extends Controller
+{
+    //
+}
